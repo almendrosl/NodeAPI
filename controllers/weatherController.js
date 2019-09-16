7bdfa21ca377eb6e2f61e6b5ca7785bb3a45873e
Lucas Matias Almendros <lucas.almendros@globant.com>
2019-09-16 17:56:34 -0300

    endpoint with de weather from the user

--- controllers/weatherController.js
+++ controllers/weatherController.js
@@ -1,9 +1,22 @@
 const weather = require('../app/weather');
+const user = require('../models/userModel');
 
 exports.weather = (req, res, next) => {
   const city = req.query.city;
   weather.getWeather(city, (err, weatherRes) => {
     if (err) return next(err);
     res.send(weatherRes);
+  });
+};
+
+exports.weatherFromUser = (req, res, next) => {
+  const id = req.params.id;
+  user.findById(id, 'favouriteCity', (err, queryRes) => {
+    if (err) return next(err);
+    const city = queryRes.favouriteCity;
+    weather.getWeather(city, (err, weatherRes) => {
+      if (err) return next(err);
+      res.send(weatherRes);
+    });
   });
 };
--- routes/weatherRouters.js
+++ routes/weatherRouters.js
@@ -5,4 +5,6 @@ const weatherController = require('../controllers/weatherController');
 /* GET weather . */
 router.get('/', weatherController.weather);
 
+router.get('/weatherFromUser/:id', weatherController.weatherFromUser);
+
 module.exports = router;
